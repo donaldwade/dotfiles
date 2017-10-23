bdf7e3959aaa2fee85b10e7bcb009fba08532be3
Donald Wade <dwade@thoughtworks.com>
2017-10-23 21:09:00 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to use
+
+1. Clone the repository
+2. Run the setup script
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+echo -e "${RED}setting up dotfiles${NC}"
+
+sudo apt -y install tig \
+  mkdir -p ~/.config \
+  cp -r tig ~/.config \
+  cp .vimrc ~ \
+  cp .tmux.conf ~
